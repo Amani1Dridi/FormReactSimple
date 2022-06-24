76c9f20077af4b0ca46f5c69d5cc4414c61e3629
Amani1Dridi <amanidrid1995@gmail.com>
2022-06-24 15:54:46 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import LoginUi from './front/LoginUi';
+
+ReactDOM.render( 
+    <LoginUi /> ,
+  document.getElementById('root')
+);
+ 
